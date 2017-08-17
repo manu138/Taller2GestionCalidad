86832921644a63b2a8dc623b94db81d2e7b5fb8a
manuela recio <manu138@hotmail.es>
2017-08-17 15:54:05 -0500

    Primer commit

---
+++ Assets/Año.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Año  {
+
+public bool esBisiesto(int año)
+    {
+        if(año % 400==0)
+        {
+            return true;
+        }
+       else if(año % 100 == 0)
+        {
+            return false;
+        }
+        else if (año % 4 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Editor/AñoTest.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class AñoTest {
+ 
+    [Test]
+	public void AñoTestSimplePasses()
+    {
+        // Arrange
+
+        Año test = new Año();
+        
+
+        //Act
+       bool esBisiesto= test.esBisiesto(5000);
+
+        //Assert
+        Assert.AreEqual(false, esBisiesto);
+	}
+        [Test]
+	public void TestSimplePasses1984()
+    {
+        // Arrange
+
+        Año test = new Año();
+        
+
+        //Act
+       bool esBisiesto= test.esBisiesto(1984);
+
+        //Assert
+        Assert.AreEqual(true, esBisiesto);
+	}
+
+    [Test]
+    public void Given1884ShouldPassIsleap()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+        bool esBisiesto = test.esBisiesto(1884);
+
+        //Assert
+        Assert.AreEqual(true, esBisiesto);
+    }
+    [Test]
+    public void CheckIfIsDivisibleBy100()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+
+        bool esBisiesto = test.esBisiesto(500);
+
+        //Assert
+        Assert.AreEqual(false, esBisiesto);
+    }
+    [Test]
+    public void CheckIfIsDivisibleBy400IsLeap()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+        bool esBisiesto = test.esBisiesto(400);
+
+        //Assert
+        Assert.AreEqual(true, esBisiesto);
+    }
+    [Test]
+    public void Given800ShouldPassIsleap()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+        bool esBisiesto = test.esBisiesto(800);
+
+        //Assert
+        Assert.AreEqual(true, esBisiesto);
+    }
+
+    [Test]
+    public void Given1600ShouldPassIsleap()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+        bool esBisiesto = test.esBisiesto(1600);
+
+        //Assert
+        Assert.AreEqual(true, esBisiesto);
+    }
+
+
+    [Test]
+    public void Given2011ShouldntPassIsntleap()
+    {
+        // Arrange
+
+        Año test = new Año();
+
+
+        //Act
+        bool esBisiesto = test.esBisiesto(2011);
+
+        //Assert
+        Assert.AreEqual(false, esBisiesto);
+    }
+}
